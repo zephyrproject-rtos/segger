38c79a447e4a47d413b4e8d34448316a5cece77c
Maureen Helm <maureen.helm@nxp.com>
2020-11-02 16:38:10 -0600

    rtt: systemview: Add linker section options
    Enables optionally placing Segger RTT and SystemView data in the DTCM
    linker section instead of the default data section. This is needed on
    SoCs in the i.MX RT series that use cacheable external SDRAM to store
    data.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

--- systemview/SEGGER_SYSVIEW_Conf.h
+++ systemview/SEGGER_SYSVIEW_Conf.h
@@ -12,6 +12,10 @@ uint32_t sysview_get_interrupt(void);
 #define SEGGER_SYSVIEW_RTT_BUFFER_SIZE CONFIG_SEGGER_SYSVIEW_RTT_BUFFER_SIZE
 #define SEGGER_SYSVIEW_POST_MORTEM_MODE CONFIG_SEGGER_SYSVIEW_POST_MORTEM_MODE
 
+#if defined(CONFIG_SEGGER_SYSVIEW_SECTION_DTCM)
+#define SEGGER_SYSVIEW_SECTION	".dtcm_data"
+#endif
+
 // Lock SystemView (nestable)
 #define SEGGER_SYSVIEW_LOCK()	{					       \
 					unsigned int __sysview_irq_key =       \
